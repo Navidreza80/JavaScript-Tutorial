3f49bca4ea33c19cb49fa64de00210c6a9300172
NavidReza <navidrezaabbaszazdeh89@gmail.com>
2025-01-06 16:41:05 +0330

    js_math tutorial

---
+++ js_math.js
@@ -0,0 +1,35 @@
+// --Math Properties--
+
+mathProps = [
+  Math.E, // returns Euler's number
+  Math.PI, // returns PI
+  Math.SQRT2, // returns the square root of 2
+  Math.SQRT1_2, // returns the square root of 1/2
+  Math.LN2, // returns the natural logarithm of 2
+  Math.LN10, // returns the natural logarithm of 10
+  Math.LOG2E, // returns base 2 logarithm of E
+  Math.LOG10E, // returns base 10 logarithm of E
+];
+
+// console.log(mathProps);
+
+// console.log(Math.round(4.6)) // == 5
+
+// console.log(Math.ceil(4.6)); // == 5
+
+// console.log(Math.floor(4.9)); // == 4
+
+// console.log(Math.trunc(4.9)); // return the integer part // == 4
+
+// console.log(Math.pow(2, 2)) // == 4
+
+// console.log(Math.sqrt(64)) // == 8
+
+// console.log(Math.abs(-4.5)) // == 4.5
+
+// console.log(Math.max(1, 2, 3)) // == 3
+
+// console.log(Math.min(1, 2, 3)) // == 1
+
+// console.log(Math.random()) // return random number between 0 and 1
+
