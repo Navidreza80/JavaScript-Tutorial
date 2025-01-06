a8fa466e131f7a8d9ef0a1bbbab2d08c8b50f8df
NavidReza <navidrezaabbaszazdeh89@gmail.com>
2025-01-06 21:34:23 +0330

    js_math updated

--- js_math.js
+++ js_math.js
@@ -27,9 +27,54 @@ mathProps = [
 
 // console.log(Math.abs(-4.5)) // == 4.5
 
+// console.log(Math.sin((90 * Math.PI) / 180));
+
+// console.log(Math.cos((0 * Math.PI) / 180));
+
 // console.log(Math.max(1, 2, 3)) // == 3
 
 // console.log(Math.min(1, 2, 3)) // == 1
 
 // console.log(Math.random()) // return random number between 0 and 1
 
+// console.log(Math.log(1));
+
+// Returns a random integer from 0 to 9:
+
+Math.floor(Math.random() * 10);
+
+// Returns a random integer from 0 to 10:
+
+Math.floor(Math.random() * 11);
+
+// Returns a random integer from 0 to 99:
+
+Math.floor(Math.random() * 100);
+
+// Returns a random integer from 0 to 100:
+
+Math.floor(Math.random() * 101);
+
+// Returns a random integer from 1 to 10:
+
+Math.floor(Math.random() * 10) + 1;
+
+// Returns a random integer from 1 to 100:
+
+Math.floor(Math.random() * 100) + 1;
+
+// This JavaScript function always returns a random number between min (included) and max (excluded):
+
+function getRndIntegerExcluded(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// console.log(getRndInteger(0, 10000)) // result == 1 - 9999
+
+// This JavaScript function always returns a random number between min and max (both included):
+
+function getRndIntegerIncluded(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// console.log(getRndInteger(0, 10000)) // result == 0 - 10000
